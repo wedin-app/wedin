40331bc29d77b65fd5a67cb0760496a15cc60872
av-dotcom <me+github@avilaca.com>
2024-09-12 15:51:15 -0400

    finishing register funcitonality

--- hooks/use-register.ts
+++ hooks/use-register.ts
@@ -1,13 +1,17 @@
-import { useState } from 'react';
+import { register } from '@/actions/auth/register';
+import { login } from '@/actions/auth/login';
+import { useState, useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { useMutation } from '@tanstack/react-query';
-import { register } from '@/actions/auth/register';
 import { RegisterSchema } from '@/schemas/auth';
+import { useToast } from '@/hooks/use-toast';
 import type { z } from 'zod';
 
 export function useRegisterForm() {
+  const { toast } = useToast();
+  const [isLoading, setIsLoading] = useState(false);
   const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+  
   const form = useForm<z.infer<typeof RegisterSchema>>({
     resolver: zodResolver(RegisterSchema),
     defaultValues: {
@@ -15,28 +19,58 @@ export function useRegisterForm() {
     },
   });
 
-  const mutation = useMutation({
-    mutationFn: (values: z.infer<typeof RegisterSchema>) => register(values),
-    onSuccess: (data) => {
-      console.log("data:", data);
-    },
-    onError: (error) => {
-      console.log("error:", error);
-    },
-  });
-
-  const handleRegister = async (values: z.infer<typeof RegisterSchema>) => {
-    const validatedFields = RegisterSchema.safeParse(values);
-    if (validatedFields.success) {
-      mutation.mutate(validatedFields.data);
+  useEffect(() => {
+    const savedEmail = localStorage.getItem('registerEmail');
+    if (savedEmail) {
+      form.setValue('email', savedEmail);
     }
-  };
+  }, [form]);
+
+  async function handleRegister(values: z.infer<typeof RegisterSchema>) {
+    console.log(values);
+    setIsLoading(true);
+    const validatedFields = RegisterSchema.safeParse(values);
+
+    if (validatedFields.success) {
+      let response = await register(validatedFields.data);
+
+      if (response?.error) {
+        toast({
+          variant: 'destructive',
+          title: 'Error al registrar usuario.',
+          description: response.error,
+        });
+
+        setIsLoading(false);
+        return null;
+      }
+
+      response = await login(
+        validatedFields.data,
+        'credentials',
+        '/onboarding'
+      );
+
+      if (response?.error) {
+        toast({
+          variant: 'destructive',
+          title: 'Error al iniciar sesión.',
+          description: response.error,
+        });
+
+        setIsLoading(false);
+        return null;
+      }
+    }
+
+    setIsLoading(false);
+  }
 
   return {
     form,
     isPasswordVisible,
     setIsPasswordVisible,
     handleRegister,
-    isLoading: mutation.isPending,
+    isLoading,
   };
 }
