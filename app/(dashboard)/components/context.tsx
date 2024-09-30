61ef8eecbd6f86cff68bdb7695542f041698801c
av-dotcom <me+github@avilaca.com>
2024-09-30 19:46:28 -0400

    fixing dashboard context

--- app/(dashboard)/components/context.tsx
+++ app/(dashboard)/components/context.tsx
@@ -1,26 +1,48 @@
 'use client';
 
-import { createContext, useContext, useState, useEffect, ReactNode } from 'react';
+import {
+  createContext,
+  useContext,
+  useState,
+  useEffect,
+  ReactNode,
+  Dispatch,
+  SetStateAction,
+} from 'react';
 import { DASHBOARD_ROUTES } from '@/utils/constants';
 
 type DashboardContextType = {
-    activeMenuItem: string;
-    setActiveMenuItem: (menuItem: string) => void;
+  activeMenuItem: string;
+  setActiveMenuItem: Dispatch<SetStateAction<string>>;
 };
 
 const DashboardContext = createContext<DashboardContextType | undefined>(undefined);
 
-export const DashboardProvider = ({ children }: { children: ReactNode }) => {
-  const [activeMenuItem, setActiveMenuItem] = useState<string>(() => {
-    const savedMenuItem = localStorage.getItem('activeMenuItem');
-    return savedMenuItem ? JSON.parse(savedMenuItem) : DASHBOARD_ROUTES.HOME;
-  });
-
+// Custom hook for local storage
+const useLocalStorage = (key: string, initialValue: string): [string, Dispatch<SetStateAction<string>>, boolean] => {
+  const [storedValue, setStoredValue] = useState<string>(initialValue);
+  const [isHydrated, setIsHydrated] = useState(false); 
   useEffect(() => {
-    if (activeMenuItem) {
-      localStorage.setItem('activeMenuItem', JSON.stringify(activeMenuItem));
+    const item = localStorage.getItem(key);
+    if (item) {
+      setStoredValue(JSON.parse(item));
     }
-  }, [activeMenuItem]);
+    setIsHydrated(true); // We are now on the client and localStorage is accessible
+  }, [key]);
+  
+  useEffect(() => {
+    if (isHydrated) {
+      localStorage.setItem(key, JSON.stringify(storedValue));
+    }
+  }, [key, storedValue, isHydrated]);
+
+  return [storedValue, setStoredValue, isHydrated];
+};
+
+export const DashboardProvider = ({ children }: { children: ReactNode }) => {
+  const [activeMenuItem, setActiveMenuItem, isHydrated] = useLocalStorage('activeMenuItem', DASHBOARD_ROUTES.HOME);
+
+  if (!isHydrated) return null;
 
   return (
     <DashboardContext.Provider value={{ activeMenuItem, setActiveMenuItem }}>
