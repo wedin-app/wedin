be1fb0cb647a2c27fd22b84e127a9038bcee952d
Jackson Huang <jacksonh201325@gmail.com>
2024-10-23 04:17:12 +0200

    Small fix for Image add and remove

--- components/dashboard/dashboard-event-details.tsx
+++ components/dashboard/dashboard-event-details.tsx
@@ -1,20 +1,21 @@
+import { getEvent } from '@/actions/data/event';
 import DashboardEventDetailsSkeleton from '@/components/skeletons/dashboard-event-details';
 import { Suspense, lazy } from 'react';
-import { getEvent } from '@/actions/data/event';
-import type { Image } from '@prisma/client';
 
-const EventDetailsUpdateForm = lazy(() => import('@/components/forms/dashboard/event-details-update-form'));
+const EventDetailsUpdateForm = lazy(
+  () => import('@/components/forms/dashboard/event-details-update-form')
+);
 
 export default async function DashboardEventDetails() {
   const event = await getEvent();
-  
+
   if (!event || 'error' in event) {
-    return <p>Error: {event.error}</p>; 
+    return;
   }
 
- return (
-    <section className="w-full h-full flex justify-start items-center flex-col gap-12">
-      <div className="w-full flex flex-col gap-4 border-b border-gray-200 pb-6">
+  return (
+    <section className="flex flex-col gap-12 justify-start items-center w-full h-full">
+      <div className="flex flex-col gap-4 pb-6 w-full border-b border-gray-200">
         <h1 className="text-2xl font-black">Presentación</h1>
         <p className="text-textTertiary">
           Haz que tu evento brille: sube fotos y cuenta de qué se trata, para
@@ -23,9 +24,7 @@ export default async function DashboardEventDetails() {
       </div>
 
       <Suspense fallback={<DashboardEventDetailsSkeleton />}>
-        <EventDetailsUpdateForm
-          event={event}
-        />
+        <EventDetailsUpdateForm event={event} />
       </Suspense>
     </section>
   );
