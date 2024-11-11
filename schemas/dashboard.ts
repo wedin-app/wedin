ca34a8a82e8e4a9778ff83f70c0f910e4fc2a861
av-dotcom <me+github@avilaca.com>
2024-11-10 22:05:58 -0300

    some refactor

--- schemas/dashboard.ts
+++ schemas/dashboard.ts
@@ -1,60 +1,66 @@
 import { z } from 'zod';
+import { EventType } from '@prisma/client';
 
-export const EventUserUpdateSchema = z.object({
-  eventDate: z.date().optional(),
-  eventType: z.string().min(1, { message: 'El tipo de evento es requerido' }),
-  name: z
-    .string()
-    .min(1, { message: 'Tu nombre no puede estar vacío' })
-    .min(2, { message: 'Nombre muy corto' })
-    .max(255, { message: 'Nombre muy largo' }),
-  lastName: z
-    .string()
-    .min(1, { message: 'Tu apellido no puede estar vacío' })
-    .min(2, { message: 'Apellido muy corto' })
-    .max(255, { message: 'Apellido muy largo' }),
-  partnerName: z
-    .string()
-    .min(1, { message: 'El nombre de tu pareja no puede estar vacío' })
-    .min(2, { message: 'Nombre muy corto' })
-    .max(255, { message: 'Nombre muy largo' })
-    .optional(),
-  partnerLastName: z
-    .string()
-    .min(1, { message: 'El apellido de tu pareja no puede estar vacío' })
-    .min(2, { message: 'Apellido muy corto' })
-    .max(255, { message: 'Apellido muy largo' })
-    .optional(),
-  partnerEmail: z
-    .string()
-    .email({ message: 'Email no válido' })
-    .optional(),
-}).superRefine((data, ctx) => {
-  // Only require partner fields if the eventType is 'wedding'
-  if (data.eventType === 'WEDDING') {
-    if (!data.partnerName) {
-      ctx.addIssue({
-        path: ['partnerName'],
-        message: 'El nombre de tu pareja es obligatorio para este tipo de evento',
-        code: z.ZodIssueCode.custom,
-      });
+export const UpdateEventAndUserFormSchema = z
+  .object({
+    eventDate: z
+      .date()
+      .refine(val => val instanceof Date && !isNaN(val.getTime()), {
+        message: 'La fecha del evento no puede estar vacía ni ser inválida',
+      }),
+    eventType: z.string(),
+    name: z
+      .string()
+      .min(1, { message: 'Tu nombre no puede estar vacío' })
+      .max(255, { message: 'Nombre muy largo' }),
+    lastName: z
+      .string()
+      .min(1, { message: 'Tu apellido no puede estar vacío' })
+      .max(255, { message: 'Apellido muy largo' }),
+    partnerName: z
+      .string()
+      .min(1, { message: 'El nombre de tu pareja no puede estar vacío' })
+      .max(255, { message: 'Nombre muy largo' })
+      .optional(),
+    partnerLastName: z
+      .string()
+      .min(1, { message: 'El apellido de tu pareja no puede estar vacío' })
+      .max(255, { message: 'Apellido muy largo' })
+      .optional(),
+    partnerEmail: z
+      .string()
+      .min(1, { message: 'El email de tu pareja no puede estar vacío' })
+      .email({ message: 'Email no válido' })
+      .optional(),
+  })
+  .superRefine((data, ctx) => {
+    if (data.eventType === EventType.WEDDING) {
+      if (!data.partnerName) {
+        ctx.addIssue({
+          path: ['partnerName'],
+          message:
+            'El nombre de tu pareja es obligatorio para este tipo de evento',
+          code: z.ZodIssueCode.custom,
+        });
+      }
+      if (!data.partnerLastName) {
+        ctx.addIssue({
+          path: ['partnerLastName'],
+          message:
+            'El apellido de tu pareja es obligatorio para este tipo de evento',
+          code: z.ZodIssueCode.custom,
+        });
+      }
+      if (!data.partnerEmail) {
+        ctx.addIssue({
+          path: ['partnerEmail'],
+          message:
+            'El email de tu pareja es obligatorio para este tipo de evento',
+          code: z.ZodIssueCode.custom,
+        });
+      }
     }
-    if (!data.partnerLastName) {
-      ctx.addIssue({
-        path: ['partnerLastName'],
-        message: 'El apellido de tu pareja es obligatorio para este tipo de evento',
-        code: z.ZodIssueCode.custom,
-      });
-    }
-    if (!data.partnerEmail) {
-      ctx.addIssue({
-        path: ['partnerEmail'],
-        message: 'El email de tu pareja es obligatorio para este tipo de evento',
-        code: z.ZodIssueCode.custom,
-      });
-    }
-  }
-});
+  });
 
 export const BankDetailsFormSchema = z.object({
   eventId: z.string(),
