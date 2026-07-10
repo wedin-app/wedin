ae652b0ead827a1728682c3f3f02425fb953487a
av-dotcom <me+github@avilaca.com>
2026-07-10 20:04:32 -0300

    ad

--- components/admin-panel/admin-panel-layout.tsx
+++ components/admin-panel/admin-panel-layout.tsx
@@ -18,7 +18,7 @@ export default function AdminPanelLayout({
       <Sidebar />
       <main
         className={cn(
-          'min-h-[calc(100vh_-_56px)] bg-zinc-50 dark:bg-zinc-900 transition-[margin-left] ease-in-out duration-300',
+          'min-h-[calc(100vh_-_56px)] bg-white dark:bg-zinc-900 transition-[margin-left] ease-in-out duration-300',
           !settings.disabled && (!getOpenState() ? 'lg:ml-[90px]' : 'lg:ml-72')
         )}
       >
